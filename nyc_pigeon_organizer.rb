c8480cd8a4666d613f62d73f04f00ef6564a0394
Emily G <aspentree79@gmail.com>
2019-08-26 12:11:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do | key, value|
+    value.each do | attributes, names |
+      names.each do | name |
+      if !hash[name]
+        hash[name] = { }
+      end
+      if !hash[name][key]
+        hash[name][key] = [ ]
+      end
+        hash[name][key] << attributes.to_s
+        end
+      end
+    end
+return hash
 end
