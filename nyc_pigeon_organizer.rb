cbafddd25c88fb8e37da4765794049add09e0bc9
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-08-29 20:35:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  list_of_names = []
+  data[:gender].values.each{|list|
+    list.each{|name|
+      if !list_of_names.include?(name)
+        list_of_names << name
+        pigeon_list[name] = {:color => [],
+        :gender => [],
+        :lives => []}
+      end}}
+      list_of_names.each{|name|
+        data.keys.each{|item|
+          paired_elems = data[item].to_a
+          paired_elems.each{|arr|
+            if arr[1].include?(name)
+              pigeon_list[name][item] << arr[0].to_s
+            end}
+          }}
+      pigeon_list
 end
