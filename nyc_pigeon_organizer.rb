2b51fd43823be4bf434d475b74ec62ea96b3b4bc
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-09 22:59:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require "pry"
+def nyc_pigeon_organizer(pigeon_data)
+  new_hash = {}
+  pigeon_data.each do |key, value|
+    value.each do |color, names|
+    binding.pry
+    end
+  end
 end
