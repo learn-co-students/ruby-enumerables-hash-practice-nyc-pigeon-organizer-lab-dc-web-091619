36d350bde619e846a0125a42854581b711ac3358
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-10 00:09:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,21 @@ require "pry"
 def nyc_pigeon_organizer(pigeon_data)
   new_hash = {}
   pigeon_data.each do |key, value|
-    value.each do |color, names|
-    binding.pry
+    value.each do |attribute, array|
+      array.each do |name|
+        
+        if new_hash[name] == nil 
+          new_hash[name] = {}
+        end 
+        if new_hash[name][key] == nil
+          new_hash[name][key] = []
+        end 
+        #   new_hash[name][key] = []
+        # else 
+        new_hash[name][key] << attribute.to_s
+        # end 
+      end 
     end
   end
+  return new_hash
 end
